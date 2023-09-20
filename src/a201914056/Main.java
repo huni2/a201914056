74a58fba3cda7e4b69984260d720a9badef12428
Heo, Changhun <powerhch@gmail.com>
2023-09-20 10:39:38 +0900

    five

--- src/a201914056/Main.java
+++ src/a201914056/Main.java
@@ -2,8 +2,9 @@ package a201914056;
 
 public class Main {
 	public static void main(String[] args) {
+		Five.print();
+		Two.print();
 		System.out.println("one 201914056");
 		Three.print();
-		Four.print();
 		}
 }
